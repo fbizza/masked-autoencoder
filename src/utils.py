77b76c8defa294ff3c4c800df2a5130cc6ec0c1e
Francesco <bizzafrancesco@gmail.com>
2025-07-08 13:22:49 +0200

    Add function to take indices from batch

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,15 @@
 import numpy as np
+import torch
+from einops import repeat
 
 def random_permutation_with_inverse(length: int):
-    """
-    Generate a random permutation of indices from 0 to length-1,
-    and compute the inverse permutation that restores the original order.
-
-    Args:
-        length (int): The length of the permutation.
-
-    Returns:
-        tuple:
-            - permutation (np.ndarray): shuffled indices.
-            - inverse_permutation (np.ndarray): indices to invert the permutation.
-    """
+    # to generate indices and invert them
     ordered_indices = np.arange(length)
     permutation = np.copy(ordered_indices)
     np.random.shuffle(permutation)
     inverse_permutation = np.argsort(permutation)
     return permutation, inverse_permutation
+
+def take_indices(sequences: torch.Tensor, indices: torch.Tensor) -> torch.Tensor:
+    # takes patches based on indices across batches
+    return torch.gather(sequences, 0, repeat(indices, 'p b -> p b c', c=sequences.shape[-1]))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-from src.utils import random_permutation_with_inverse
+import torch
+
+from src.utils import random_permutation_with_inverse, take_indices
 
 
 def test_random_permutation_with_inverse():
@@ -13,3 +15,32 @@ def test_random_permutation_with_inverse():
     recovered = forward[backward]
     print("Recovered (should be ordered):", recovered)
     assert np.array_equal(recovered, np.arange(length)), "The inversion does not work"
+
+def test_take_indices():
+    # shape: (4, 2, 1)
+    sequences = torch.tensor([
+        [[0], [10]],
+        [[1], [11]],
+        [[2], [12]],
+        [[3], [13]],
+    ])
+
+    # indices to reorder patches per batch, shape: (4, 2)
+    indices = torch.tensor([
+        [3, 2],
+        [2, 3],
+        [1, 1],
+        [0, 0],
+    ])
+
+    output = take_indices(sequences, indices)
+
+    print("\nInput sequences:\n", sequences.squeeze(-1))
+    print("Indexes:\n", indices)
+    print("Output sequences:\n", output.squeeze(-1))
+
+    expected_batch0 = torch.tensor([3, 2, 1, 0])
+    expected_batch1 = torch.tensor([12, 13, 11, 10])
+
+    assert torch.equal(output[:, 0, 0], expected_batch0), "Batch 0 mismatch"
+    assert torch.equal(output[:, 1, 0], expected_batch1), "Batch 1 mismatch"
