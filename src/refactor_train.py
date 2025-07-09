a822deba331eefc43f46b20a257a5cfec96242bc
Francesco <bizzafrancesco@gmail.com>
2025-07-09 15:08:20 +0200

    Complete training function

--- src/refactor_train.py
+++ src/refactor_train.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 import math
 import torch
 import torchvision
@@ -7,9 +6,9 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor, Compose, Normalize
 from tqdm import tqdm
 from einops import rearrange
-
 from src.model.mae import MaskedAutoencoder
-from utils import set_seed
+from utils import set_seed, load_config
+
 
 def get_cifar10_dataloaders(train_subset=None, load_batch_size=64):
     transform = Compose([ToTensor(), Normalize(0.5, 0.5)])
@@ -23,99 +22,104 @@ def get_cifar10_dataloaders(train_subset=None, load_batch_size=64):
         train = train_full
 
     train_loader = torch.utils.data.DataLoader(
-        train, batch_size=load_batch_size, shuffle=True, num_workers=4,
-        pin_memory=True, persistent_workers=True, drop_last=True
+        train, batch_size=load_batch_size, shuffle=True
     )
     val_loader = torch.utils.data.DataLoader(
-        val, batch_size=load_batch_size, shuffle=False, num_workers=2,
-        pin_memory=True, drop_last=False
+        val, batch_size=load_batch_size, shuffle=False
     )
     return train_loader, val_loader, val
 
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, default=29)
-    parser.add_argument('--batch_size', type=int, default=2048)
-    parser.add_argument('--max_device_batch_size', type=int, default=64)
-    parser.add_argument('--base_learning_rate', type=float, default=1.5e-4)
-    parser.add_argument('--weight_decay', type=float, default=0.05)
-    parser.add_argument('--mask_ratio', type=float, default=0.75)
-    parser.add_argument('--total_epoch', type=int, default=200)
-    parser.add_argument('--warmup_epoch', type=int, default=10)
-    parser.add_argument('--model_path', type=str, default='data/weights/vit-t-mae.pt')
-    parser.add_argument('--train_subset', type=int, default=None,
-                        help='Numero di immagini da usare per il training (default: tutte)')
-
-    args = parser.parse_args()
-    set_seed(args.seed)
-
-    batch_size = args.batch_size
-    load_batch_size = min(args.max_device_batch_size, batch_size)
-    assert batch_size % load_batch_size == 0
-    steps_per_update = batch_size // load_batch_size
-
-    dataloader, val_loader, val_dataset = get_cifar10_dataloaders(args.train_subset, load_batch_size)
-
-    writer = SummaryWriter(os.path.join('data/logs', 'cifar10', 'mae-pretrain'))
+    config = load_config(config_name="default")
+    set_seed(config.seed)
+    batch_size = config.batch_size
+    gpu_load_batch_size = min(config.max_device_batch_size, batch_size)
+    assert batch_size % gpu_load_batch_size == 0, \
+        f"batch_size ({batch_size}) must be divisible by gpu_load_batch_size ({gpu_load_batch_size})"
+    steps_per_update = batch_size // gpu_load_batch_size
+    dataloader, val_loader, val_dataset = get_cifar10_dataloaders(config.train_subset, gpu_load_batch_size)
+    writer = SummaryWriter(os.path.join('data/logs', 'cifar10', f'{config.config_name}'))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f"Using device: {device}")
 
-    model = MaskedAutoencoder(mask_ratio=args.mask_ratio).to(device)
-    optim = torch.optim.AdamW(model.parameters(),
-                              lr=args.base_learning_rate * args.batch_size / 256,
-                              betas=(0.9, 0.95),
-                              weight_decay=args.weight_decay)
+    model = MaskedAutoencoder(mask_ratio=config.mask_ratio).to(device)
 
-    lr_func = lambda epoch: min((epoch + 1) / (args.warmup_epoch + 1e-8),
-                                0.5 * (math.cos(epoch / args.total_epoch * math.pi) + 1))
+    optim = torch.optim.AdamW(model.parameters(),
+                              lr=config.base_learning_rate * config.batch_size / 256,
+                              betas=(0.9, 0.95),
+                              weight_decay=config.weight_decay)
+
+    lr_func = lambda epoch: min(
+                                (epoch + 1) / (config.warmup_epoch + 1e-8),  # warm up
+                                0.5 * (math.cos(epoch / config.total_epoch * math.pi) + 1)  # cosine Decay
+                                )
+
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func)
 
-    step_count = 0
+    step_count = 0  # for gradient accumulation
     optim.zero_grad()
-    for e in range(args.total_epoch):
+    for e in range(config.total_epoch):
         print(f'Epoch {e}:')
         model.train()
-        losses = []
+        train_losses = []
         for img, _ in tqdm(dataloader):
             step_count += 1
             img = img.to(device)
             predicted_img, mask = model(img)
-            loss = torch.mean((predicted_img - img) ** 2 * mask) / args.mask_ratio
+            loss = torch.mean((predicted_img - img) ** 2 * mask) / config.mask_ratio  # NOTE: no loss is computed on visible patches!! also note that loss is computed on normalized pixels
             loss.backward()
             if step_count % steps_per_update == 0:
                 optim.step()
                 optim.zero_grad()
-            losses.append(loss.item())
+            train_losses.append(loss.item())
         lr_scheduler.step()
-        avg_loss = sum(losses) / len(losses)
-        print(f'Train loss on masked pixels: {avg_loss:.6f}')
+        avg_train_loss = sum(train_losses) / len(train_losses)
+        print(f'Train loss on masked pixels: {avg_train_loss:.6f}')
 
-        ''' Visualizzazione immagini '''
+        # To visualize improvement on images reconstruction quality for each epoch
         model.eval()
         with torch.no_grad():
-            val_img = torch.stack([val_dataset[i][0] for i in range(16)])
-            val_img = val_img.to(device)
-            predicted_val_img, mask = model(val_img)
-            predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
-            img = torch.cat([val_img * (1 - mask), predicted_val_img, val_img], dim=0)
+            test_img = torch.stack([val_dataset[i][0] for i in range(16)])
+            test_img = test_img.to(device)
+            predicted_val_img, mask = model(test_img)
+            predicted_val_img = predicted_val_img * mask + test_img * (1 - mask)
+            img = torch.cat([test_img * (1 - mask), predicted_val_img, test_img], dim=0)
             img = rearrange(img, '(v h1 w1) c h w -> c (h1 h) (w1 v w)', w1=2, v=3)
             writer.add_image('mae_image', (img + 1) / 2, global_step=e)
 
-        ''' Calcolo della loss di validazione '''
-        val_losses = []
+        # validation loss
+        test_losses = []
         with torch.no_grad():
-            for val_img, _ in val_loader:
-                val_img = val_img.to(device)
-                predicted_val_img, mask = model(val_img)
-                loss = torch.mean((predicted_val_img - val_img) ** 2 * mask) / args.mask_ratio
-                val_losses.append(loss.item())
-        avg_val_loss = sum(val_losses) / len(val_losses)
+            for test_img, _ in val_loader:
+                test_img = test_img.to(device)
+                predicted_val_img, mask = model(test_img)
+                loss = torch.mean((predicted_val_img - test_img) ** 2 * mask) / config.mask_ratio
+                test_losses.append(loss.item())
+        avg_test_loss = sum(test_losses) / len(test_losses)
 
         writer.add_scalars('loss', {
-            'train': avg_loss,
-            'test': avg_val_loss
+            'train': avg_train_loss,
+            'test': avg_test_loss
         }, global_step=e)
-        print(f'Val loss on masked pixels: {avg_val_loss:.6f}')
+        print(f'Test loss on masked pixels: {avg_test_loss:.6f}')
 
-        ''' Salvataggio del modello '''
-        torch.save(model, args.model_path)
+        # save model every 10 epochs
+        if e % 10 == 0:
+            os.makedirs(config.model_path, exist_ok=True)
+            torch.save(model, f"{config.model_path}/{config.config_name}_epoch_{e}.pt")
+
+        # save check point every 25 epochs
+        if e % 25 == 0:
+            checkpoint = {
+                'epoch': e,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optim.state_dict(),
+                'lr_scheduler_state_dict': lr_scheduler.state_dict()
+            }
+            os.makedirs(config.model_path, exist_ok=True)
+            torch.save(checkpoint, f"{config.model_path}/{config.config_name}_checkpoint_epoch_{e}.pt")
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import torch
 import random
+import os
+import yaml
 from einops import repeat
+from types import SimpleNamespace
 
 def set_seed(seed=29):
     random.seed(seed)
@@ -9,6 +12,20 @@ def set_seed(seed=29):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
+
+
+def load_config(config_name="default"):
+    config_path = os.path.join(os.path.dirname(__file__), "..", "config.yaml")
+    with open(config_path, "r") as f:
+        all_cfgs = yaml.safe_load(f)
+    cfg_dict = all_cfgs.get(config_name)
+    if cfg_dict is None:
+        raise ValueError(f"Config '{config_name}' not found in {config_path}")
+
+    cfg_dict["config_name"] = config_name
+    cfg_dict["model_path"] = os.path.join("data", "weights", f"{config_name}")
+
+    return SimpleNamespace(**cfg_dict)
 
 def random_permutation_with_inverse(length: int):
     # to generate indices and invert them
@@ -21,3 +38,4 @@ def random_permutation_with_inverse(length: int):
 def take_indices(sequences: torch.Tensor, indices: torch.Tensor) -> torch.Tensor:
     # takes patches based on indices across batches
     return torch.gather(sequences, 0, repeat(indices, 'p b -> p b c', c=sequences.shape[-1]))
+
